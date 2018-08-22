0efb7560036874e33212cfe8cde18d2224330c91
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2018-08-22 16:57:29 +0700

    Сайт картона
    Сайт картона

---
+++ carton/js/script_2.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('<link rel="stylesheet" href="yved/style.css">').appendTo('head');var i=0;function yved(){i=1;$('.yved:nth-child('+i+')').fadeIn(500).delay(7000).fadeOut(500);}setTimeout(function(){setInterval(function(){i=i+1;if(i>10)i=1;$('.yved:nth-child('+i+')').fadeIn(500).delay(7000).fadeOut(500);},60000);yved();},7000);});
