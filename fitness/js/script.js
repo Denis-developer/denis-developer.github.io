71cc4164f861300c8f013ba15b2c6385507d0981
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2018-09-17 08:03:36 +0700

    Сайт для фитнеса
    Сайт для фитнеса

---
+++ fitness/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+	let a = document.getElementsByTagName('a'),
+			form = document.getElementsByTagName('form')[0],
+			humburger = document.getElementsByClassName('humburger'),
+			menu = document.querySelector('.menu');
+
+	for(let i = 0; i < a.length; i++){
+		a[i].addEventListener('click', function (argument) {
+			event.preventDefault();
+		});
+	 } // Отмена действия ссылок по умолчанию
+	 
+	 form.addEventListener('submit', function (argument) {
+	 	event.preventDefault();
+	 }); // Отмена отправки формы
+
+	 for(var i = 0; i < humburger.length; i++){
+				humburger[i].addEventListener('click', function () {
+					this.classList.toggle('active');
+					menu.classList.toggle('dn');
+				});
+			}
+	
+});
