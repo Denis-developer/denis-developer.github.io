fee9ff52b8f5525d8943903adfe709d156edcdbd
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2018-10-26 07:04:50 +0700

    Add files via upload

---
+++ 2/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.main-slider').slick({
+  	dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="prev"><img src="img/arrow-prev.png" alt="" /></button>',
+    nextArrow: '<button class="next"><img src="img/arrow-next.png" alt="" /></button>'
+  });
+});
