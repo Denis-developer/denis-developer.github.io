78c74b7f36df0aced08f02722ed6c7cac6bb6549
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2018-10-27 17:41:18 +0700

    Add files via upload

--- 8/js/main.js
+++ 8/js/main.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
     slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow: '<button class="prev"><img src="img/arrow-prev.png" alt="" /></button>',
-    nextArrow: '<button class="next"><img src="img/arrow-next.png" alt="" /></button>'
+    nextArrow: '<button class="next"><img src="img/arrow-next.png" alt="" /></button>',
+	  responsive: [
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        arrows: false
+	      }
+	    }
+	  ]
   });
 });
