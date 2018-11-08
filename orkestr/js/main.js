4dac00cb873a7d6eda935a39c21e3c164d1bdcaa
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2018-11-08 20:52:23 +0700

    Add files via upload

---
+++ orkestr/js/main.js
@@ -0,0 +1,60 @@
+(function($) {
+$(function() {
+
+  // TABS
+
+	let musicImg = document.getElementsByClassName('music-img');
+ 
+  $('ul.poster-list').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('.poster').find('div.poster-month').removeClass('active-block').eq($(this).index()).addClass('active-block');
+  });
+  $('ul.music-tool__list').on('click', 'li:not(.active-tool)', function() {
+    $(this)
+      .addClass('active-tool').siblings().removeClass('active-tool')
+      .closest('.music').find('div.music-block').removeClass('active-block').eq($(this).index()).addClass('active-block');
+  		let num = this.getAttribute('data-num');
+      for(let z = 0; z < musicImg.length; z++){
+          musicImg[z].classList.remove('active-img');
+        if(musicImg[z].getAttribute('data-num') == num)
+          musicImg[z].classList.add('active-img');
+        }
+  });
+
+  $('.media-tabs').on('click', function() {
+    $(this)
+      .closest('.media').find('div.media-blockTab').removeClass('active-block').eq($(this).index()).addClass('active-block');
+  });
+
+  // ЯКОРЯ
+
+    $('.header-menu__link').on('click', function(event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+      top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 800);
+    });
+
+    // SLICK
+
+    $('.comment-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows : false,
+      autoplay: true,
+      dots: true
+    });
+
+    $('.slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      prevArrow: '<button class="prev"><img src="img/slider/arrow-prev.png" alt="" /></button>',
+      nextArrow: '<button class="next"><img src="img/slider/arrow-next.png" alt="" /></button>'
+    }); 
+ 
+});
+})(jQuery);
+
