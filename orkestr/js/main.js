40239ed8535f5726973a9aeb1cc2204362cdfd39
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2018-11-09 18:54:16 +0700

    Add files via upload

--- orkestr/js/main.js
+++ orkestr/js/main.js
@@ -3,7 +3,9 @@ $(function() {
 
   // TABS
 
-	let musicImg = document.getElementsByClassName('music-img');
+	let musicImg = document.getElementsByClassName('music-img'),
+      humburger = document.getElementsByClassName('humburger'),
+      menu = document.querySelector('.menu');
  
   $('ul.poster-list').on('click', 'li:not(.active)', function() {
     $(this)
@@ -27,9 +29,18 @@ $(function() {
       .closest('.media').find('div.media-blockTab').removeClass('active-block').eq($(this).index()).addClass('active-block');
   });
 
+  // АНИМАЦИЯ ГАМБУРГЕРА
+
+    for (var i = 0; i < humburger.length; i++) {
+        humburger[i].addEventListener('click', function() {
+            this.classList.toggle('active-hum');
+            menu.classList.toggle('dn');
+        });
+    }
+
   // ЯКОРЯ
 
-    $('.header-menu__link').on('click', function(event) {
+    $('.header-menu__link, .menu__link').on('click', function(event) {
       event.preventDefault();
       var id = $(this).attr('href'),
       top = $(id).offset().top;
