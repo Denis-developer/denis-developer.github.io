ce0f1824b23d2f2687e0a4508ac6eaeea1b2c7d3
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2018-11-30 16:14:50 +0700

    Add files via upload

---
+++ mobile/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+	let btn1 = document.querySelector('.btn-submit'),
+		  btn2 = document.querySelector('.btn-back'),
+		  input = document.getElementsByTagName('input');
+
+	for(let i = 0;i < input; i++){
+		if(input[i].onfocus){
+			btn1.style.display = 'none';
+			btn2.style.display = 'none';
+		}
+	}
+})
