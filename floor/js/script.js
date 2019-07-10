1f8fe93eae709a3860af00aa852d744e0660b78b
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2019-07-10 22:28:23 +0700

    Add files via upload

--- floor/js/script.js
+++ floor/js/script.js
@@ -7,18 +7,33 @@ $(document).ready(function(){
 	  slidesToScroll: 3,
   	responsive: [
 	    {
-        breakpoint: 768,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 4,
           slidesToScroll: 2
         }
       },
       {
-	      breakpoint: 576,
-	      settings: {
-	        slidesToShow: 2
-	      }
-	    }
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 425,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
   	]
   });
 });
