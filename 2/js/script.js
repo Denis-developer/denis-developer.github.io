f1fe1481d0552179f526e81a7cb61ba30aee036b
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2019-08-09 15:58:26 +0700

    Add files via upload

---
+++ 2/js/script.js
@@ -0,0 +1,74 @@
+jQuery(document).ready(function($) {
+	
+	$('.slider').slick({
+	  infinite: false,
+	  slidesToShow: 3,
+  	  centerMode: true,
+  	  speed: 200,
+  	  swipe: false,
+  	  centerPadding: '20px',
+  	  prevArrow: '<button class="slick-prev"></button>',
+	  nextArrow: '<button class="slick-next"></button>',
+	  autoplay: false,
+	  slidesToScroll: 1,
+	  responsive: [
+	    {
+	      breakpoint: 992,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 576,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows: false,
+  	  		centerMode: false,
+	        swipe: true
+	      }
+	    }
+	  ]
+	});
+
+	var sliderBlock = document.getElementsByClassName('slider-block'),
+		sliderNumber = document.querySelector('.slider-number'),
+		activeNumber = document.querySelector('.active-number'),
+		number = 0,
+		active = 1;
+
+	for( let i = 0; i < sliderBlock.length; i++){
+		number++;
+	}
+
+	$('.slick-prev').css('display', 'none');
+	// Первый слайдер
+	$('.slick-next').on('click', function(event) {
+		++active;
+		activeNumber.innerHTML = active;
+		if(activeNumber.innerHTML == number){
+			$('.slick-next').css('display', 'none');
+		}
+		else if(activeNumber.innerHTML > '1'){
+			$('.slick-prev').css('display', 'block');
+		}
+	});
+
+	$('.slick-prev').on('click', function(event) {
+		--active;
+		activeNumber.innerHTML = active;
+		if(activeNumber.innerHTML == '1'){
+			$('.slick-prev').css('display', 'none');
+		}
+		else if(activeNumber.innerHTML < number){
+			$('.slick-next').css('display', 'block');
+		}
+	});
+
+	sliderNumber.innerHTML = number;
+
+
+	
+
+});
