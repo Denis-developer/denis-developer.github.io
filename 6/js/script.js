936698bcde350bae2c0b7ef0cd7489bb6f4de5e7
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2019-08-12 14:25:26 +0700

    Add files via upload

---
+++ 6/js/script.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	// Якоря
+	$('.link-down').on('click', function(event) {
+          event.preventDefault();
+          var id = $(this).attr('href'),
+          top = $(id).offset().top;
+          $('body,html').animate({scrollTop: top}, 800);
+      });
+});
