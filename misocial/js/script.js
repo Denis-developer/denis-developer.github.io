bfde998b9a3dc03105b847506b61e9d919fcff5c
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2020-01-03 18:58:56 +0700

    Add files via upload

--- misocial/js/script.js
+++ misocial/js/script.js
@@ -4,6 +4,19 @@ window.addEventListener('DOMContentLoaded', function(){
       menu = document.querySelector('.menu'),
       menuLink = document.getElementsByClassName('menu__link');
 
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event){
+      event.preventDefault();
+      const blockID = anchor.getAttribute("href");
+      document.querySelector('' + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
+
   // АНИМАЦИЯ ГАМБУРГЕРА
     humburger.addEventListener('click', function() {
         this.classList.toggle('active');
