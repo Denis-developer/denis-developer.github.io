30b4cdc02869995c10c0c3966f3de0a5a6d0353b
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2020-04-28 14:04:23 +0700

    Add files via upload

--- Sportshold/js/script.js
+++ Sportshold/js/script.js
@@ -1,40 +1,25 @@
 window.addEventListener('DOMContentLoaded', function() {
   // ТАБЫ
     let tab = document.getElementsByClassName('tab'),
-    		tabContent1 = document.getElementsByClassName('product-wrapper'),
-    		tabContent2 = document.getElementsByClassName('recall-text'),
+    		tabContent = document.getElementsByClassName('product-wrapper'),
     		info = document.getElementsByClassName('tabs');
 
     // СКРЫТИЕ ВСЕХ ТАБОВ КРОМЕ ПЕРВОГО
-  	function hideTabContent1(a) {
-  		for(let i = a; i < tabContent1.length; i++){
-  			tabContent1[i].classList.remove('show');
-  			tabContent1[i].classList.add('hide');
+  	function hideTabContent(a) {
+  		for(let i = a; i < tabContent.length; i++){
+  			tabContent[i].classList.remove('show');
+  			tabContent[i].classList.add('hide');
   		};
   	};
 
-  	function hideTabContent2(a) {
-  		for(let i = a; i < tabContent2.length; i++){
-  			tabContent2[i].classList.remove('db');
-  			tabContent2[i].classList.add('hide');
-  		};
-  	};
 
-  	hideTabContent1(1);
-  	hideTabContent2(1);
+  	hideTabContent(1);
 
-  	function showTabContent1(b) {
-  		if(tabContent1[b].classList.contains('hide')){
-  			hideTabContent1(0);
-  			tabContent1[b].classList.remove('hide');
-  			tabContent1[b].classList.add('show');
-  		};
-  	};
-  	function showTabContent2(b) {
-  		if(tabContent2[b].classList.contains('hide')){
-  			hideTabContent2(0);
-  			tabContent2[b].classList.remove('hide');
-  			tabContent2[b].classList.add('db');
+  	function showTabContent(b) {
+  		if(tabContent[b].classList.contains('hide')){
+  			hideTabContent(0);
+  			tabContent[b].classList.remove('hide');
+  			tabContent[b].classList.add('show');
   		};
   	};
 
@@ -49,28 +34,17 @@ window.addEventListener('DOMContentLoaded', function() {
     						tab[z].classList.remove('product-tab-active');
     					}
     					tab[i].classList.add('product-tab-active');
-    					showTabContent1(i);
+    					showTabContent(i);
     					break;
     				};
     			};
     		}
-    		else if(target.className == 'tab recall-tab'){
-    			for(let i = 0; i < tab.length; i++){
-    				if(target == tab[i]){
-    					for (let z = 0; z < tab.length; z++){
-    						tab[z].classList.remove('recall-tab_active');
-    					}
-    					tab[i].classList.add('recall-tab_active');
-    					showTabContent2(i-3);
-    					break;
-    				};
-    			};
-    		};
     	});
     };
 
     let menuBtn = document.querySelector('.menu-btn'),
-        mobileMenu = document.querySelector('.mobile-menu');
+        mobileMenu = document.querySelector('.mobile-menu'),
+        mobileMenuLinks = document.getElementsByClassName('mobile-menu__link');
 
     menuBtn.addEventListener('click', function(event) {
       event.preventDefault();
@@ -81,12 +55,22 @@ window.addEventListener('DOMContentLoaded', function() {
       else{
         mobileMenu.style.transform = 'scale(0)';
       }
-
     });
 
+    for (var i = 0; i < mobileMenuLinks.length; i++) {
+      mobileMenuLinks[i].addEventListener('click', function(event) {
+        mobileMenu.style.transform = 'scale(0)';
+        menuBtn.classList.toggle('menu-btn_active');
+      });
+    }
 
+    // 3D CARD EFFECT
+    var card = $(".product-block");
 
-
-
+    $('.product').on("mousemove",function(e) {
+      var ax = -($('.product').innerWidth()/5- e.pageX)/20;
+      var ay = ($('.product').innerHeight()/5- e.pageY)/60;
+      card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+    });
 
 });
