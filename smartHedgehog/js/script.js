b1c0e380ec58fa3b3ba6ff2937e04b27ed11ed1b
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2020-06-07 17:59:18 +0700

    Add files via upload

--- smartHedgehog/js/script.js
+++ smartHedgehog/js/script.js
@@ -16,7 +16,8 @@ window.addEventListener('DOMContentLoaded', function() {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 1,
+          dots: true
         }
       }
     ]
@@ -51,9 +52,69 @@ window.addEventListener('DOMContentLoaded', function() {
           slidesToShow: 2,
           arrows: false
         }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 4,
+          arrows: false
+        }
       }
     ]
   });
 
   $(".contact-form__tel").mask("+7 (999)999-99-99");
+
+  // HEADER FIXED
+  let header = $('.header');
+  const min1200 = window.matchMedia( "(min-width: 1200px)" );
+
+  document.addEventListener('scroll', function (e) {
+    if(min1200.matches){
+      if(document.documentElement.scrollTop >= 50){
+        header.css({
+          'position': 'fixed',
+          'top': '0px',
+          'background': '#fff'
+        });
+      }
+      else{
+        header.css({
+          'position': 'absolute',
+          'top': '50px',
+          'background': 'transparent'
+        });
+      }
+    }
+    else{
+      header.css({
+        'position': 'fixed',
+        'top': '0px',
+        'background': '#fff'
+      });
+    }
+  })
+
+  let menuBtn = document.querySelector('.menu-btn'),
+      mobileMenu = document.querySelector('.mobile-menu'),
+      mobileMenuLinks = document.getElementsByClassName('mobile-menu__link');
+
+  menuBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    this.classList.toggle('menu-btn_active');
+    if(this.classList.contains('menu-btn_active')){
+      mobileMenu.style.transform = 'scale(1)';
+    }
+    else{
+      mobileMenu.style.transform = 'scale(0)';
+    }
+  });
+
+  for (var i = 0; i < mobileMenuLinks.length; i++) {
+    mobileMenuLinks[i].addEventListener('click', function(event) {
+      mobileMenu.style.transform = 'scale(0)';
+      menuBtn.classList.toggle('menu-btn_active');
+    });
+  }
+
 });
