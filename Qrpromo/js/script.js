34280e9402e6845b63f97a71b3c393acc86ee9d0
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2020-07-23 14:41:38 +0700

    Add files via upload

--- Qrpromo/js/script.js
+++ Qrpromo/js/script.js
@@ -13,15 +13,19 @@ window.addEventListener('DOMContentLoaded', function() {
     this.classList.toggle('menu-btn_active');
     if(this.classList.contains('menu-btn_active')){
       mobileMenu.style.transform = 'scale(1)';
-      header.classList.toggle('popup-open');
       document.getElementsByTagName('html')[0].style.overflowY = "hidden";
-      // Перекраска логотипа в чёрный цвет
-      for (var i = 0; i < logoSt0.length; i++) {
-        logoSt0[i].style.stroke = '#000';
-      }
-      for (var i = 0; i < logoSt1.length; i++) {
-        logoSt1[i].style.fill = '#000';
-      }
+      setTimeout(() => {
+        header.classList.toggle('popup-open');
+      },400)
+      setTimeout(() => {
+        // Перекраска логотипа в чёрный цвет
+        for (var i = 0; i < logoSt0.length; i++) {
+          logoSt0[i].style.stroke = '#000';
+        }
+        for (var i = 0; i < logoSt1.length; i++) {
+          logoSt1[i].style.fill = '#000';
+        }
+      }, 200)
     }
     else{
       mobileMenu.style.transform = 'scale(0)';
