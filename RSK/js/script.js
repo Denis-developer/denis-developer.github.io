567ead3d65a403568d563f3fa5b451af1e03d76e
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2021-03-02 16:17:36 +0700

    Add files via upload

---
+++ RSK/js/script.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+  const swiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    }
+  });
+
+  const swiper1 = new Swiper('.sertificates-container', {
+    speed: 400,
+    slidesPerView: 6,
+    slidesPerGroup: 1,
+    pagination: {
+      el: '.sertificates-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      },
+      600: {
+        slidesPerView: 3,
+        slidesPerGroup: 2
+      },
+      820: {
+        slidesPerView: 4,
+        slidesPerGroup: 2
+      },
+      992: {
+        slidesPerView: 5,
+        slidesPerGroup: 1
+      },
+      1200: {
+        slidesPerView: 6,
+        slidesPerGroup: 1
+      }
+    }
+  });
+
+});
