c0164492013576913b30c2c9b5d6c5e05d85fbae
Denis-developer <42605443+Denis-developer@users.noreply.github.com>
2021-03-08 17:07:01 +0700

    Add files via upload

--- alxp/js/script.js
+++ alxp/js/script.js
@@ -50,7 +50,49 @@ window.addEventListener('DOMContentLoaded', function() {
   	jQuery(this).click(function(){
 			jQuery(this).parent().toggleClass('active');
 		});
-
 	});
+
+  // ТАБЫ
+    let tab = document.getElementsByClassName('skills-tab__item'),
+    		tabContent = document.getElementsByClassName('skills-content'),
+    		info = document.getElementsByClassName('skills-tab');
+
+    // СКРЫТИЕ ВСЕХ ТАБОВ КРОМЕ ПЕРВОГО
+  	function hideTabContent(a) {
+  		for(let i = a; i < tabContent.length; i++){
+  			tabContent[i].classList.remove('show');
+  			tabContent[i].classList.add('hide');
+  		};
+  	};
+
+
+  	hideTabContent(1);
+
+  	function showTabContent(b) {
+  		if(tabContent[b].classList.contains('hide')){
+  			hideTabContent(0);
+  			tabContent[b].classList.remove('hide');
+  			tabContent[b].classList.add('show');
+  		};
+  	};
+
+    for (var i = 0; i < info.length; i++) {
+      info[i].addEventListener('click', function (event) {
+    		let target = event.target;
+    		event.preventDefault();
+    		if(target.className == 'skills-tab__item'){
+    			for(let i = 0; i < tab.length; i++){
+    				if(target == tab[i]){
+    					for (let z = 0; z < tab.length; z++){
+    						tab[z].classList.remove('active-tab');
+    					}
+    					tab[i].classList.add('active-tab');
+    					showTabContent(i);
+    					break;
+    				};
+    			};
+    		}
+    	});
+    };
 
 });
