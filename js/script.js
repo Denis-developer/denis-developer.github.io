8532a7cffb96e6392927e39e84f714f2036badb6
Denis Fedorov <patron.2001denden@mail.ru>
2023-02-20 12:36:13 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,42 +1,32 @@
-window.addEventListener('DOMContentLoaded', function() {
-  let mainHeight = document.querySelector('.main').clientHeight,
-      blockCircle = document.getElementsByClassName('about-block__circle');
+document.addEventListener('DOMContentLoaded', function () {
 
+    const isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
 
-  window.addEventListener('scroll', function() {
-    if(window.pageYOffset >= mainHeight){
-      for (var i = 0; i < blockCircle.length; i++) {
-        blockCircle[i].style.animation = "fadeShadow 6s infinite 3s";
-      }
+    if (isMobile.any()) {
+        let projectsWrapper = document.querySelectorAll('.projects-block__wrapper');
+        for (let i = 0; i < projectsWrapper.length; i++) {
+            projectsWrapper[i].style.transform = "translateY(0)";
+        }
     }
-  });
 
-  // CURSOR
-  const cursor = document.querySelector('.cursor');
-
-  document.addEventListener('mousemove', e => {
-      cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
-  });
-
-  document.addEventListener('click', e => {
-      cursor.classList.add("expand");
-      setTimeout(() => {
-          cursor.classList.remove("expand");
-      }, 500);
-  });
-
-  $(window).bind('scroll',function(e){
-    parallaxScroll();
-  });
-
-  function parallaxScroll(){
-      var scrolled = $(window).scrollTop();
-      $('.main-title').css('top',(0-(scrolled*.50))+'px');
-  }
-
-  SmoothScroll({ stepSize: 40 });
-
-  new WOW().init();
-
-
-});
+    SmoothScroll({ stepSize: 40 });
+})
