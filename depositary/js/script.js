0fa35db21614172e5796ad270dde9024d2fadfe3
Denis Fedorov <patron.2001denden@mail.ru>
2023-07-05 23:58:54 +0700

    Add files via upload

--- depositary/js/script.js
+++ depositary/js/script.js
@@ -6,7 +6,6 @@ for (let i = 0; i < accordions.length; i++) {
         viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         accordions[i].classList.toggle('active');
         const accordionContent = accordions[i].nextElementSibling;
-        console.log(accordionContent.scrollHeight);
         if (accordionContent.style.maxHeight == "" && viewport_width > 576) {
             accordionContent.style.maxHeight = accordionContent.scrollHeight + 64 + "px";
             accordionContent.style.padding = "32px";
@@ -82,5 +81,52 @@ const menuMobile = document.querySelector('.header__top .container');
 menuBurger.addEventListener('click', function (e) {
     document.body.classList.toggle('lock')
     menuBurger.classList.toggle('active');
-    menuMobile.classList.toggle('active');
+    menuMobile.classList.toggle('show');
 })
+
+function smoothScrollToAnchor(anchor) {
+    viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    let offset = 50;
+    if(viewport_width < 577) {
+        offset = 80;
+    }
+    const target = document.querySelector(anchor);
+    if (!target) return;
+    
+    const targetPosition = target.offsetTop - offset;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 800;
+  
+    let start = null;
+  
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      const progress = timestamp - start;
+      const percentage = Math.min(progress / duration, 1);
+      const easing = easeInOutQuad(percentage);
+      window.scrollTo(0, startPosition + distance * easing);
+      if (progress < duration) {
+        requestAnimationFrame(step);
+      } else {
+        history.replaceState(null, null, anchor); // Заменяем URL без изменения положения на странице
+      }
+    }
+  
+    function easeInOutQuad(t) {
+      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    }
+  
+    requestAnimationFrame(step);
+  }
+  
+  let linkNav = document.querySelectorAll('[href^="#"]');
+  for (let i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      const hash = this.getAttribute('href');
+      smoothScrollToAnchor(hash);
+    });
+  }
+  
+  
