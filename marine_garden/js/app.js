585744c23ad767f7797b9f9e083da96bab98d803
Denis Fedorov <patron.2001denden@mail.ru>
2023-08-02 00:45:28 +0700

    Add files via upload

---
+++ marine_garden/js/app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Проверка поддержки webp, добавление класса webp или no-webp для html
+    function isWebp() {
+        function testWebP(callback) {
+            // Проверка поддержки webp
+            var webP = new Image();
+            webP.onload = webP.onerror = function () {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+
+        // Добавление класаа
+        testWebP(function (support) {
+
+            if (support == true) {
+                document.querySelector('body').classList.add('webp');
+            } else {
+                document.querySelector('body').classList.add('no-webp');
+            }
+        });
+    }
+
+    isWebp();
+
+    const header = document.querySelector(".header");
+    let scrollPos = window.scrollY;
+
+    if(scrollPos > 10) {
+        header.classList.add('active');
+    }
+
+    document.addEventListener("scroll", function () {
+        scrollPos = window.scrollY;
+        if (scrollPos > 0) {
+            header.classList.add('active');
+        } else {
+            header.classList.remove('active');
+        }
+    });
+
+})
