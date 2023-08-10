268e551112ac0826e13cc45d9d76e59eba2077ae
Denis Fedorov <patron.2001denden@mail.ru>
2023-08-10 19:25:36 +0700

    Update app.js

--- adyghe_state_uni/js/app.js
+++ adyghe_state_uni/js/app.js
@@ -294,33 +294,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     })
 
-        (function () {
-            // проверяем поддержку
-            if (!Element.prototype.closest) {
-                // реализуем
-                Element.prototype.closest = function (css) {
-                    var node = this;
-
-                    while (node) {
-                        if (node.matches(css)) return node;
-                        else node = node.parentElement;
-                    }
-                    return null;
-                };
-            }
-        })();
-
-    (function () {
-        // проверяем поддержку
-        if (!Element.prototype.matches) {
-            // определяем свойство
-            Element.prototype.matches = Element.prototype.matchesSelector ||
-                Element.prototype.webkitMatchesSelector ||
-                Element.prototype.mozMatchesSelector ||
-                Element.prototype.msMatchesSelector;
-        }
-    })();
-
 
 
 })
