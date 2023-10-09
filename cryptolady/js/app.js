0d6cb1f7f8662218e51efe86a827101626a4694c
Denis Fedorov <patron.2001denden@mail.ru>
2023-10-09 19:22:49 +0700

    Add files via upload

--- cryptolady/js/app.js
+++ cryptolady/js/app.js
@@ -53,7 +53,51 @@ document.addEventListener('DOMContentLoaded', function () {
         thumbs: {
             swiper: swiper4,
         },
-        
+
     });
+
+    function smoothScrollToAnchor(anchor) {
+        let viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        let offset = 0;
+        const target = document.querySelector(anchor);
+        if (!target) return;
+
+        const targetPosition = target.offsetTop - offset;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 800;
+
+        let start = null;
+
+        function step(timestamp) {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+            const percentage = Math.min(progress / duration, 1);
+            const easing = easeInOutQuad(percentage);
+            window.scrollTo(0, startPosition + distance * easing);
+            if (progress < duration) {
+                requestAnimationFrame(step);
+            } else {
+                history.replaceState(null, null, anchor); // Заменяем URL без изменения положения на странице
+            }
+        }
+
+        function easeInOutQuad(t) {
+            return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+        }
+
+        requestAnimationFrame(step);
+    }
+
+    let linkNav = document.querySelectorAll('[href^="#"]');
+    for (let i = 0; i < linkNav.length; i++) {
+        linkNav[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            const hash = this.getAttribute('href');
+            smoothScrollToAnchor(hash);
+        });
+    }
+
+
 
 })
